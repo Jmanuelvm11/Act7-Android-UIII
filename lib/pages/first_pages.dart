3681a1b3ad0affcd9b73ed710df86c81cf08ffd5
FlutLab.io <>
2021-06-02 16:26:03 +0000

    Initial commit

---
+++ lib/pages/first_pages.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image.asset("assets/images/-dental_.jpg"),
+      ), // center
+    ); //container
+  } // widget
+} // first page
